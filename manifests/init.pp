5bb38feec439103e8e451411d6af175da6f41590
Adam Breaux <breauxaj@gmail.com>
2013-12-24 14:04:29 -0500

    fill in

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,12 @@
 class ruby {
+  $required = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [
+      'ruby',
+      'ruby-rdoc',
+      'rubygems'
+    ]
+  }
+
+  package { $required: ensure => latest }
 
 }
