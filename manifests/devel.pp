ff68b97cc411b1ba904edf649da2201fac062f07
Adam Breaux <breauxaj@gmail.com>
2014-03-16 18:44:31 -0400

    whitespace

--- manifests/devel.pp
+++ manifests/devel.pp
@@ -8,7 +8,7 @@ class ruby::devel (
   $depends = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'ruby' ],
   }
-  
+
   package { $required:
     ensure  => $ensure,
     require => Package[$depends],
